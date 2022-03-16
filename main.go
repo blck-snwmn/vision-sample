a46f2fa3423934e8b29d347ce04153ad53685746
blck-snwmn <whitesnowmancreator@gmail.com>
2022-03-17 01:14:48 +0900

    Add main.go

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	vision "cloud.google.com/go/vision/apiv1"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("command line args is not exist")
+	}
+	filename := os.Args[1]
+
+	// Creates a client.
+	client, err := vision.NewImageAnnotatorClient(context.Background())
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("failed to read file: %v", err)
+	}
+	defer file.Close()
+
+	image, err := vision.NewImageFromReader(file)
+	if err != nil {
+		log.Fatalf("failed to create image: %v", err)
+	}
+
+	text, err := client.DetectDocumentText(context.Background(), image, nil)
+	if err != nil {
+		log.Fatalf("failed to detect document text: %v", err)
+	}
+	fmt.Println(text.Text)
+}
